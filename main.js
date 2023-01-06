b93afe9b3167dbbc3e8aa1861ee9a473fed44a7e
CorwinCheung <85517581+CorwinCheung@users.noreply.github.com>
2023-01-06 18:48:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function questionAnswer() {
+    var question = document.getElementById("question").value;
+    var answer = document.getElementById("answer").value;
+    
+    document.getElementById("response").innerHTML = answer;
+}
